880ac96d3a84de76dd7d7f3615603db35f1893bf
Thethan <eagleking495@gmail.com>
2021-12-04 16:44:08 +0630

    fixed variable name port to PORT

--- index.js
+++ index.js
@@ -87,4 +87,4 @@ app.post("/api/persons", (req, res) => {
   persons = persons.concat(newPerson);
 });
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Server is listening on port ${port}`));
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
